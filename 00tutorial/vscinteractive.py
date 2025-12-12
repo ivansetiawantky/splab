855cf746b43fb430eaabfb44bb8abf75aab88892
Ivan Setiawan <ivan@arcadia.co.jp>
2025-12-12 16:46:45 +0900

    Add jupyter package, vscode container extension and setting

---
+++ 00tutorial/vscinteractive.py
@@ -0,0 +1,16 @@
+# From https://code.visualstudio.com/docs/python/jupyter-support-py
+# The `# %%` marks a cell
+
+# %%
+msg = "Hello World"
+print(msg)
+
+# %%
+msg = "Hello again"
+print(msg)
+
+# %%
+print(512)
+
+# View->Command palette: >Jupyter: Create Interactive Window
+# View->Command palette: >Jupyter: Run Current File in Interactive Window
