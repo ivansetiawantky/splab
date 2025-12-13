7fb4f74bc28ec69dd4fb99253c48e0a82ccbd6e7
Ivan Setiawan <ivans@yahoo.co.jp>
2025-12-13 21:25:23 +0900

    Markdown, include LaTeX, prepend with sharp character are previewed

---
+++ 00tutorial/graphsample.md
@@ -0,0 +1,22 @@
+
+# Required vscode extensions
+
+* Markdown Preview Enhanced
+* Markdown All in One
+
+# Example of DOT
+
+```dot
+digraph G {
+  A -> B
+  B -> C
+  B -> D
+}
+```
+
+# Example of mermaid that is NOT rendered with offline html (MUST USE CDN HOSTED)
+
+```mermaid
+sequenceDiagram
+  Alice ->> Bob: Hello Bob
+```
--- 00tutorial/vscinteractive.py
+++ 00tutorial/vscinteractive.py
@@ -6,6 +6,9 @@ msg = "Hello World"
 print(msg)
 
 # %%
+# MARK: Testing print(msg)
+#
+# The above will be shown if minimap is enabled.
 msg = "Hello again"
 print(msg)
 
@@ -13,4 +16,29 @@ print(msg)
 print(512)
 
 # View->Command palette: >Jupyter: Create Interactive Window
+#     Press Shift+Enter for newline. Press Enter to execute.
+#
 # View->Command palette: >Jupyter: Run Current File in Interactive Window
+
+# %% [markdown]
+# # 線形計画法
+# ## subsection
+# * グラフ描画
+
+# %% [markdown]
+# This is an inline math: $\LaTeX$, $v = f \cdot \lambda$. <br>
+# This is a displayed math:
+# $$
+# \int_{-\infty}^{\infty} f(x) \mathrm{d} x.
+# $$ <br>
+# This is also displayed math:
+# $$
+# \int_{1}^{\infty} \frac{1}{x^2} \mathrm{d}x = 1
+# $$
+# $$
+# E = mc^2 \tag{2002}
+# $$
+# This is also a displayed mathematics:
+# $$
+# \frac{\mu_0}{4\pi} \times \frac{i \mathrm{d}l \sin \theta}{r^2}
+# $$
